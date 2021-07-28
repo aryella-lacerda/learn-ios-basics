7a1683d0d55a8a7d00669deebc91f1da4c7e35d4
Aryella Lacerda <aryella.lacerda@superlogica.com>
2021-07-28 10:00:17 -0300

    chore: work in progress

--- NightWatch/DetailsScreen.swift
+++ NightWatch/DetailsScreen.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct DetailsScreen: View {
   @Binding var character: Character
+  @Environment(\.verticalSizeClass) var verticalSize
+
   
   var body: some View {
     VStack {
       Text(character.name)
+      if verticalSize == .regular {
+        Text("""
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed nibh placerat nulla posuere dictum. Ut iaculis quam id augue euismod dapibus. Mauris in purus sed quam interdum gravida. Sed mattis eu dui ac fermentum. Nam quis aliquam dui, posuere elementum arcu. Aliquam tristique sem rhoncus vulputate tincidunt. Suspendisse vitae sapien volutpat, consectetur lacus at, tincidunt ex. Donec ac turpis mattis, tincidunt felis imperdiet, suscipit justo. Mauris mattis lobortis arcu, ac vulputate enim condimentum eget. Pellentesque id libero justo. Maecenas nec velit et tortor imperdiet convallis ut ac ipsum. Nam sed erat at eros egestas fringilla. Curabitur varius justo tellus, nec fringilla ante cursus et. Duis euismod ex vitae pellentesque mollis.
+          """
+        ).padding(.all)
+      }
       Button("Toggle Favorite", action: {
         character.isFavorite = !character.isFavorite
       })
